05fd85689401dabb85277036a391ba95d95ad28d
francescotrudu <francescotrudu84@gmail.com>
2023-02-09 18:00:09 +0100

    Add home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,1 +1,14 @@
 @extends('layouts.app')
+@section('main')
+<section id="under-construction">
+    <div class="container">
+        <div class="row text-center">
+            <div class="col-12">
+                <h1 class="fs-1">Page under construction</h1>
+                <h6>Click on <a class="text-decoration-none" href="{{route('movies')}}">movies</a></h6>
+            </div>
+        </div>
+    </div>
+
+</section>
+@endsection
