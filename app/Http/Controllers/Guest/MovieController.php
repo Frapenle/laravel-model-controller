3469b4f7957d9daa82daf15799ffe712d26d62bf
francescotrudu <francescotrudu84@gmail.com>
2023-02-09 16:15:09 +0100

    Add MovieController

---
+++ app/Http/Controllers/Guest/MovieController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers\Guest;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class MovieController extends Controller
+{
+    //qui creo un metodo
+    public function index()
+    {
+        return view('home');
+    }
+}
