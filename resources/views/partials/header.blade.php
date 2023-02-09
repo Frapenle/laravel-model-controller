a6eaca42380b26323c827ead11f150b9c166bfaa
francescotrudu <francescotrudu84@gmail.com>
2023-02-09 17:05:16 +0100

    Edit nav links

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,11 +1,11 @@
 <header>
     <nav class="container">
-        <div class="logo al">
-            {{-- <a href="{{ route('home') }}"><img src="{{ Vite::asset('resources/assets/images/dc-logo.png')}}" alt="DC logo"></a> --}}
+        <div class="logo">
+            
         </div>
         <ul>
-            {{-- <li><a href="{{route('home')}}">Home</a></li>
-            <li><a href="{{route('comics')}}">comics</a></li> --}}
+            <li><a href="#">Home</a></li>
+            <li><a href="#">Movies</a></li>
             <li><a href="#">Contacts</a></li>
         </ul>
         <div></div>
