6d24e7655265f1481c83b654338a0f3df9c1a240
francescotrudu <francescotrudu84@gmail.com>
2023-02-09 18:01:41 +0100

    Add route home, edit route movie

--- routes/web.php
+++ routes/web.php
@@ -14,4 +14,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', [MovieController::class, 'index']);
+Route::get('/movies', [MovieController::class, 'index'])->name('movies');
+
+Route::get('/', function () {
+    return view('home');
+})->name('home');
