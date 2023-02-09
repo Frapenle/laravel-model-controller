e75d90be1acc68589d15123c633400a5d9c5355d
francescotrudu <francescotrudu84@gmail.com>
2023-02-09 18:00:59 +0100

    Add footer

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,18 @@
+<footer class="">
+    <section class="footer-top">
+            <section class="footer-bottom">
+                <div class="container">
+                    <div class="footer-right">
+                        <div class="social-network">
+                            <h3>Follow us</h3>
+                            <a href="#" class=""><img src="{{ Vite::asset('resources/assets/images/footer-facebook.png')}}" alt="Facebook"></a>
+                            <a href="#" class=""><img src="{{ Vite::asset('resources/assets/images/footer-twitter.png')}}" alt="Twitter"></a>
+                            <a href="#" class=""><img src="{{ Vite::asset('resources/assets/images/footer-youtube.png')}}" alt="YouTube"></a>
+                            <a href="#" class=""><img src="{{ Vite::asset('resources/assets/images/footer-pinterest.png')}}" alt="Pinterest"></a>
+                            <a href="#" class=""><img src="{{ Vite::asset('resources/assets/images/footer-periscope.png')}}" alt="Periscope"></a>
+                        </div>
+                    </div>
+                </div>
+            </section>
+    </footer>
+</footer>
